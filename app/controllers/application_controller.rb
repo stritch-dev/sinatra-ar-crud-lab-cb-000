0f3d570c53120263db5559b225d98ad77afd03b0
stritch.dev <stritch.dev@gmail.com>
2018-04-04 10:56:18 -0500

    in progress not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  ####### create ###### 
+  get '/posts/new' do
+    erb :new
+  end
 
+  post '/posts' do 
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  ####### read ###### 
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  ####### update ###### 
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  ####### delete ###### 
+  get '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :index
+    
   end
 end
